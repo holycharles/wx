333bc319c6f660ace9da1bc4cc0fb4cc900999c7
zallhy@DESKTOP-PSK6ERV <zallhy@DESKTOP-PSK6ERV>
2017-09-27 16:34:49 +0800

    我的  我的活动页面

---
+++ pages/my/activity.js
@@ -0,0 +1,103 @@
+// pages/my/activity.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    activityList: [
+      {
+        advimg: "http://p3.wmpic.me/article/2017/09/19/1505800793_ifRRQepR_215x185.jpg",
+        name: "地中海的金色畅想——以色列爱乐铜管音乐会",
+        price: "¥90，",
+        num:"1份",
+        adult:"成人费用，",
+        time: "9月20日  周三 18:50",
+        finishimg:""
+      },
+      {
+        advimg: "http://p3.wmpic.me/article/2017/09/19/1505800793_ifRRQepR_215x185.jpg",
+        name: "武汉站 俄罗斯芭蕾国家剧院芭蕾舞《天鹅湖》",
+        price: "¥180，",
+        num: "2份",
+        adult: "成人费用，",
+        time: "9月28日  周四 19:00",
+        finishimg: ""
+      },
+      {
+        advimg: "http://p3.wmpic.me/article/2017/09/19/1505800793_ifRRQepR_215x185.jpg",
+        name: "地中海的金色畅想——以色列爱乐铜管音乐会",
+        price: "¥90，",
+        num: "1份",
+        adult: "成人费用，",
+        time: "9月20日  周三 18:50",
+        finishimg: ""
+      },
+      {
+        advimg: "http://p3.wmpic.me/article/2017/09/19/1505800793_ifRRQepR_215x185.jpg",
+        name: "武汉站 俄罗斯芭蕾国家剧院芭蕾舞《天鹅湖》",
+        price: "¥180，",
+        num: "2份",
+        adult: "成人费用，",
+        time: "9月28日  周四 19:00",
+        finishimg: "/images/my/my_act_icon_finish.png"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
